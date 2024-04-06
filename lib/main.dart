c5d1010c72478ead0fbce5c659fd23ffff2bc492
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-04-06 15:45:49 +0700

    style: add dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,44 @@ import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
-  seedColor: const Color.fromARGB(255, 69, 59, 181),
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
 );
 
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        colorScheme: kDarkColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kDarkColorScheme.onPrimaryContainer,
+          foregroundColor: kDarkColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+        // textTheme: ThemeData().textTheme.copyWith(
+        //       titleLarge: TextStyle(
+        //         fontWeight: FontWeight.normal,
+        //         color: kDarkColorScheme.onSecondaryContainer,
+        //         fontSize: 16,
+        //       ),
+        //     ),
+      ),
       theme: ThemeData().copyWith(
+        useMaterial3: true,
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
           backgroundColor: kColorScheme.onPrimaryContainer,
@@ -31,6 +62,7 @@ void main() {
               ),
             ),
       ),
+      // themeMode: ThemeMode.system, // Default
       home: const Expenses(),
     ),
   );
